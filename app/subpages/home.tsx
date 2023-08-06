7ef76054e52acd338d9337740026bbd99c9f18d0
Jacob Husband <jakehusband2@gmail.com>
2023-08-06 16:58:57 -0700

    add potential fix for mobile shop link

--- app/subpages/home.tsx
+++ app/subpages/home.tsx
@@ -2,10 +2,24 @@ import Image from "next/image";
 import profile from "/public/fb_marketplace.jpg";
 
 const handleClick = () => {
-  window.open(
-    "https://www.facebook.com/marketplace/profile/100052202858703/?ref=permalink&mibextid=dXMIcH",
-    "_blank"
-  );
+  var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+  if (isSafari) {
+    var timeout = setTimeout(function () {
+      window.location.href = "https://apps.apple.com/app/facebook/id284882215"; // Facebook's App Store URL
+    }, 500);
+
+    window.location.href =
+      "https://www.facebook.com/marketplace/profile/100052202858703/?ref=permalink&mibextid=dXMIcH"; // Attempt to open Facebook app
+
+    window.addEventListener("pagehide", function () {
+      clearTimeout(timeout); // Clear the timeout if the app opened successfully
+    });
+  } else {
+    window.open(
+      "https://www.facebook.com/marketplace/profile/100052202858703/?ref=permalink&mibextid=dXMIcH"
+    );
+  }
 };
 
 export default function Home() {
