e5dac7437f9181e91e5246585e48fd466053ed2b
Jacob Husband <jakehusband2@gmail.com>
2023-08-05 17:43:35 -0700

    add image lightbox

--- app/content.tsx
+++ app/content.tsx
@@ -10,7 +10,22 @@ import sofa3 from "../public/sofa_3.jpg";
 import sofa4 from "../public/sofa_4.jpg";
 import sofa5 from "../public/sofa_5.jpg";
 
+import LightGalleryComponent from "lightgallery/react";
+
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
+
+import lgThumbnail from "lightgallery/plugins/thumbnail";
+import lgZoom from "lightgallery/plugins/zoom";
+import type { LightGallery } from "lightgallery/lightgallery";
+import { useRef } from "react";
+import { GalleryItem } from "lightgallery/lg-utils";
+
 const sofas = [sofa1, sofa2, sofa3, sofa4, sofa5];
+const sofaSources: GalleryItem[] = sofas.map((sofa) => {
+  return { src: sofa.src, thumb: sofa.src };
+});
 
 const breakpointColumnsObj = {
   default: 4,
@@ -20,6 +35,8 @@ const breakpointColumnsObj = {
 };
 
 export default function Content() {
+  const lightboxRef = useRef<LightGallery | null>(null);
+
   return (
     <Tab.Panels className="h-full bg-opacity-80 max-w-[900px] w-full p-2 sm:p-4 my-6">
       <Tab.Panel>
@@ -28,37 +45,32 @@ export default function Content() {
           className="flex gap-4"
           columnClassName=""
         >
-          <Image
-            src={sofa1}
-            placeholder="blur"
-            className="my-4"
-            alt="sofa-1"
-          ></Image>
-          <Image
-            src={sofa2}
-            placeholder="blur"
-            className="my-4"
-            alt="sofa-2"
-          ></Image>
-          <Image
-            src={sofa3}
-            placeholder="blur"
-            className="my-4"
-            alt="sofa-3"
-          ></Image>
-          <Image
-            src={sofa4}
-            placeholder="blur"
-            className="my-4"
-            alt="sofa-4"
-          ></Image>
-          <Image
-            src={sofa5}
-            placeholder="blur"
-            className="my-4"
-            alt="sofa-5"
-          ></Image>
+          {sofas.map((sofa, index) => (
+            <Image
+              key={sofa.src}
+              src={sofa}
+              placeholder="blur"
+              className="my-4 hover:opacity-90 cursor-pointer"
+              alt="sofa"
+              onClick={() => {
+                lightboxRef.current?.openGallery(index);
+              }}
+            />
+          ))}
         </Masonry>
+        <div className="App">
+          <LightGalleryComponent
+            onInit={(ref) => {
+              if (ref) {
+                lightboxRef.current = ref.instance;
+              }
+            }}
+            speed={500}
+            plugins={[lgThumbnail, lgZoom]}
+            dynamic
+            dynamicEl={sofaSources}
+          ></LightGalleryComponent>
+        </div>
       </Tab.Panel>
       <Tab.Panel>Browser insert for facebook marketplace inventory</Tab.Panel>
       <Tab.Panel>Contact information</Tab.Panel>
